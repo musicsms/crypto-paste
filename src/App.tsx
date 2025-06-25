e017084c8baf6f988dc10abc759a2b3e20a15ac2
Bopbap <bopbap0402@gmail.com>
2025-06-25 22:38:57 +0700

    update Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 import { CreatePaste } from './pages/CreatePaste'
 import { ViewPaste } from './pages/ViewPaste'
 import { Home } from './pages/Home'
@@ -7,15 +8,16 @@ import { Home } from './pages/Home'
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors">
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors flex flex-col">
         <Header />
-        <main className="container mx-auto px-4 py-8">
+        <main className="container mx-auto px-4 py-8 flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<CreatePaste />} />
             <Route path="/:id" element={<ViewPaste />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   )
